8f9f930d37a917b71a6534f4b7440ac2243a71dc
Ajay Kumar <ajkumar_25@yahoo.com>
2013-11-30 10:48:45 +0530

    heroku

--- app/__init__.py
+++ app/__init__.py
@@ -6,19 +6,7 @@ from flask.ext.migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
-import psycopg2
-import urlparse
-
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/images')
